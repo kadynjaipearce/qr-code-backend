3ebe0874be863e90c9f2f55cf507421e954b96e5
Kadyn-Jai Pearce <kadynjaipearce@gmail.com>
2024-10-10 19:20:39 +0800

    🎉 build: start project.

---
+++ src/encoding.rs
@@ -0,0 +1,3 @@
+pub fn convert_string_into_bytes(input: &str) -> Vec<u8> {
+    input.as_bytes().to_vec()
+}
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod encoding;
+mod matrix;
+mod output;
+use rocket::{get, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_rocket::ShuttleRocket {
+    let rocket = rocket::build().mount("/", routes![index]);
+
+    Ok(rocket.into())
+
+}
+
